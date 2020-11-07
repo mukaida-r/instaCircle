e76dd99c74c57beb4fccdddd0b3b7ed52468e40e
mitsui <m.mihye1128@gmail.com>
2020-11-07 13:11:27 +0900

    feat(image-list):画像一覧UI作成

--- src/app/event/event/image-list/image-list.component.ts
+++ src/app/event/event/image-list/image-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Image } from 'src/app/interfaces/image';
 
 @Component({
   selector: 'app-image-list',
@@ -6,6 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./image-list.component.scss'],
 })
 export class ImageListComponent implements OnInit {
+  imageList: Image[] = [
+    {
+      imageURL: 'assets/images/image-card-sample01.jpg',
+    },
+    {
+      imageURL: 'assets/images/image-card-sample02.jpg',
+    },
+    {
+      imageURL: 'assets/images/image-card-sample03.jpg',
+    },
+    {
+      imageURL: 'assets/images/image-card-sample04.jpg',
+    },
+    {
+      imageURL: 'assets/images/image-card-sample01.jpg',
+    },
+    {
+      imageURL: 'assets/images/image-card-sample02.jpg',
+    },
+    {
+      imageURL: 'assets/images/image-card-sample03.jpg',
+    },
+    {
+      imageURL: 'assets/images/image-card-sample04.jpg',
+    },
+  ];
   constructor() {}
 
   ngOnInit(): void {}
