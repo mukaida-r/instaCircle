0c385b5148e144e27ada7a949fcf0f61bf3100f9
yuuki-kouno <zgoovw@gmail.com>
2020-11-08 16:34:13 +0900

    feat(イメージ詳細画面): 画面作成途中

--- src/app/event/event/image-detail/image-detail.component.ts
+++ src/app/event/event/image-detail/image-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Image } from 'src/app/interfaces/image';
+import { ImageService } from 'src/app/services/image.service';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-image-detail',
@@ -6,7 +10,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./image-detail.component.scss'],
 })
 export class ImageDetailComponent implements OnInit {
-  constructor() {}
+  eventId: string = this.route.snapshot.paramMap.get('eventId');
+  imageId: string = this.route.snapshot.paramMap.get('imageId');
+  image$ = this.imageService.getImage(this.eventId, this.imageId);
+  constructor(
+    private imageService: ImageService,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log(this.eventId, this.imageId, this.image$);
+  }
 }
