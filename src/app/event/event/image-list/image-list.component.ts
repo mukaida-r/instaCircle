59c1640e75a7dd66ff9a406d6dc7a39341b7532f
peyo-code <mr233425@gmail.com>
2020-11-08 12:06:12 +0900

    fix(image-list): snapshotからlocation.hrefに変更

--- src/app/event/event/image-list/image-list.component.ts
+++ src/app/event/event/image-list/image-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Image } from 'src/app/interfaces/image';
-import { EventService } from 'src/app/services/event.service';
 import { ImageService } from 'src/app/services/image.service';
 
 @Component({
@@ -10,10 +9,9 @@ import { ImageService } from 'src/app/services/image.service';
   styleUrls: ['./image-list.component.scss'],
 })
 export class ImageListComponent implements OnInit {
-  private test = this.route.snapshot.paramMap.get('eventId');
   eventId: string;
   imageList: Image[];
-  value = 'http://localhost:4200/events/' + this.test;
+  value = location.href;
 
   constructor(
     private route: ActivatedRoute,
