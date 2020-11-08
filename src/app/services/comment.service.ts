7cdd0cad378fbb8877f33fd891454aa4288409b7
yuuki-kouno <zgoovw@gmail.com>
2020-11-08 16:33:42 +0900

    feat(comment.service): createComment()追加

--- src/app/services/comment.service.ts
+++ src/app/services/comment.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore/firestore';
+import { AuthService } from './auth.service';
+import * as firebase from 'firebase';
+import { Event } from '../interfaces/event';
+import { Image } from '../interfaces/image';
+import { UserService } from './user.service';
+import { User } from '../interfaces/user';
+import { Comment } from '../interfaces/comment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CommentService {
-  constructor() {}
+  constructor(private db: AngularFirestore, private authService: AuthService) {}
+
+  createComment(user: User, event: Event, image: Image, comment: Comment) {
+    const id: string = this.db.createId();
+    return this.db
+      .doc<Comment>(
+        `events/${event.eventId}/images/${image.imageId}/comments/${id}`
+      )
+      .set({
+        userName: user.name,
+        uid: user.uid,
+        commentBody: comment.commentBody,
+        imageURL: comment.imageURL,
+        avatarURL: user.avatarURL,
+        id,
+        eventId: event.eventId,
+        createdAt: firebase.default.firestore.Timestamp.now(),
+      });
+  }
 }
