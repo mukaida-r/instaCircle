c65439207717da617196f2c38531ec315bb09d81
mitsui <m.mihye1128@gmail.com>
2020-11-08 12:54:38 +0900

    feat(event):イベント詳細画面

--- src/app/event/event/event.component.ts
+++ src/app/event/event/event.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Event } from 'src/app/interfaces/event';
+import { AuthService } from 'src/app/services/auth.service';
 import { EventService } from 'src/app/services/event.service';
 
 @Component({
@@ -10,14 +11,19 @@ import { EventService } from 'src/app/services/event.service';
   styleUrls: ['./event.component.scss'],
 })
 export class EventComponent implements OnInit {
+  uid: string;
   event$: Observable<Event>;
 
   constructor(
     private route: ActivatedRoute,
-    private eventServise: EventService
+    private eventServise: EventService,
+    private authServise: AuthService
   ) {
     this.route.paramMap.subscribe((params) => {
       this.event$ = this.eventServise.getEvent(params.get('eventId'));
+    });
+    this.authServise.user$.subscribe((user) => {
+      this.uid = user.uid;
     });
   }
 
