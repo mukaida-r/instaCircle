dbf01ddd6ae7b3df953bf464c27b1e9beab0f7d6
yuuki-kouno <zgoovw@gmail.com>
2020-11-08 16:29:08 +0900

    feat(image-detail): routing追加

--- src/app/event/event-routing.module.ts
+++ src/app/event/event-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EventComponent } from './event/event.component';
 import { PostImagesComponent } from './post-images/post-images.component';
+import { ImageDetailComponent } from './event/image-detail/image-detail.component';
 
 const routes: Routes = [
   {
@@ -15,6 +16,10 @@ const routes: Routes = [
   {
     path: ':eventId/post-images',
     component: PostImagesComponent,
+  },
+  {
+    path: ':eventId/images/:imageId',
+    component: ImageDetailComponent,
   },
 ];
 
