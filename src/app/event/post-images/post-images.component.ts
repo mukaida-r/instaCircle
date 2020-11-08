a1c828f68e7f35cf807d03fed0282f8005ab6678
Yusuke Gunji <inochideutae@gmail.com>
2020-11-08 13:15:43 +0900

    feat(post-imege): editimagedialog追加

--- src/app/event/post-images/post-images.component.ts
+++ src/app/event/post-images/post-images.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
+import { CropperOptions } from '@deer-inc/cropper';
 import { Observable } from 'rxjs';
 import { EventService } from 'src/app/services/event.service';
 import { ImageService } from 'src/app/services/image.service';
+import { EditImageDialogComponent } from '../edit-image-dialog/edit-image-dialog.component';
 
 @Component({
   selector: 'app-post-images',
@@ -10,15 +13,21 @@ import { ImageService } from 'src/app/services/image.service';
   styleUrls: ['./post-images.component.scss'],
 })
 export class PostImagesComponent implements OnInit {
+  readonly MAX_IMAGE_FILE_LENGTH: number = 4;
+
   event$: Observable<Event>;
   eventId: string;
+  imageFile: string;
   images: any[] = [];
   imageFiles: (File | string)[] = [];
+  settingImage: string;
+  settingImages: string[];
 
   constructor(
     private route: ActivatedRoute,
     private eventServise: EventService,
-    private imageService: ImageService
+    private imageService: ImageService,
+    private dialog: MatDialog
   ) {
     this.route.paramMap.subscribe((params) => {
       this.eventId = params.get('eventId');
@@ -34,5 +43,14 @@ export class PostImagesComponent implements OnInit {
 
   uploadImages() {
     this.imageService.uploadImages(this.eventId, this.images);
+  }
+
+  openEditImageDialog() {
+    this.dialog.open(EditImageDialogComponent, {
+      maxWidth: '100vw',
+      minWidth: '50%',
+      autoFocus: false,
+      restoreFocus: false,
+    });
   }
 }
