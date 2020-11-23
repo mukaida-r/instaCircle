a106a0b75c7e7dd74e9d42f58bbbf156630a6ee4
Yusuke Gunji <inochideutae@gmail.com>
2020-11-23 14:17:08 +0900

    feat(event-list-card): joinedUserCount追加

--- src/app/home/home/join-event-dialog/join-event-dialog.component.ts
+++ src/app/home/home/join-event-dialog/join-event-dialog.component.ts
@@ -4,6 +4,7 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { Event } from 'src/app/interfaces/event';
 import { User } from 'src/app/interfaces/user';
 import { AuthService } from 'src/app/services/auth.service';
 import { EventService } from 'src/app/services/event.service';
@@ -21,6 +22,9 @@ export class JoinEventDialogComponent implements OnInit {
   isProcessing: boolean;
   isPossible: boolean;
   eventId: string;
+  event: Event;
+
+  event$: Observable<Event> = this.eventService.getEvent(this.data.id);
 
   get password(): FormControl {
     return this.passwordForm.get('password') as FormControl;
@@ -38,7 +42,11 @@ export class JoinEventDialogComponent implements OnInit {
     }
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.event$.subscribe((event) => {
+      this.event = event;
+    });
+  }
 
   async submit(uid: string) {
     this.isPossible = await this.eventService.judgePassword(
@@ -47,6 +55,7 @@ export class JoinEventDialogComponent implements OnInit {
     );
     if (this.isPossible) {
       this.userService.joinEvent(this.data.id, uid);
+      this.event.joinedUserCount++;
       this.router.navigateByUrl(`event/${this.data.id}`);
     } else {
       this.snackBar.open('パスワードが違います');
