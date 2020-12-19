a53bd2205bea42f3893a4409dc96e131025f907f
peyo-code <mr233425@gmail.com>
2020-12-19 18:34:16 +0900

    feat(delete-account-dialog): 退会ボタン押下時にモーダル を表示する。

--- src/app/settingth/settingth/settingth.component.ts
+++ src/app/settingth/settingth/settingth.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DeleteAccountDialogComponent } from 'src/app/delete-account-dialog/delete-account-dialog.component';
 
 @Component({
   selector: 'app-settingth',
@@ -6,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settingth.component.scss'],
 })
 export class SettingthComponent implements OnInit {
-  constructor() {}
+  constructor(private dialog: MatDialog) {}
 
   ngOnInit(): void {}
+
+  openDeleteAccountDialog() {
+    this.dialog.open(DeleteAccountDialogComponent, {
+      width: '400px',
+      autoFocus: false,
+      restoreFocus: false,
+    });
+  }
 }
