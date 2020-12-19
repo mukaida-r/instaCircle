34aedcbf4340bbea36c4a9a3d2e115ba884f33eb
kokiinoue45 <koki_inoue45@icloud.com>
2020-12-20 00:36:22 +0900

    editコンポーネントにフォームガード実装

--- src/app/event/edit/edit.component.ts
+++ src/app/event/edit/edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
@@ -15,6 +15,7 @@ export class EditComponent implements OnInit {
   readonly titleMaxLength: number = 40;
   readonly descliptionMaxLength: number = 40;
 
+  isComplete: boolean;
   uid: string;
   event$: Observable<Event>;
   eventId: string;
@@ -58,11 +59,20 @@ export class EditComponent implements OnInit {
     });
   }
 
+  @HostListener('window:beforeunload', ['$event'])
+  unloadNotification($event: any) {
+    if (this.form.dirty) {
+      $event.preventDefault();
+      $event.returnValue = '作業中の内容が失われますがよろしいですか？';
+    }
+  }
+
   onCroppedImage(image: string) {
     this.imageFile = image;
   }
 
   async updateEventData() {
+    this.isComplete = true;
     this.processing = true;
     let eventData: Omit<Event, 'eventId' | 'ownerId' | 'createAt'> = this.form
       .value;
